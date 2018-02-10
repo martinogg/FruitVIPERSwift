9122462055808025e67c4bcb3ae660b416629cc5
Martin Ogg <2martinogg@gmail.com>
2018-02-10 13:35:13 +0000

    initial Commit

---
+++ FruitVIPER/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FruitVIPER
+//
+//  Created by martin ogg on 10/02/2018.
+//  Copyright © 2018 martinogg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
